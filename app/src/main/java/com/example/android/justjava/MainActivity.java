c3a416edea390556325314774eb503fbad07ff6e
sbh <saeedibnhamad@gmail.com>
2016-12-26 09:16:11 +0300

    Add + and - feature to quantity.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -19,29 +19,43 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    int quantity = 2;
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-
-        int quantity = 2;
-
-
-        display(quantity);
         displayPrice(quantity * 5);
     }
 
+    /**
+     * this method will increment the quantity
+     */
+    public void increment(View view) {
+
+        quantity++;
+        displayQuantity(quantity);
+    }
+
+    /**
+     * this method will decrement the quantity
+     */
+    public void decrement(View view) {
+        quantity--;
+        displayQuantity(quantity);
+    }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
